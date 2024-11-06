4d8f58730da146de66e5cf61cd8709a33b4c8016
олегсий <78318213+lex4z@users.noreply.github.com>
2024-11-06 19:30:21 +0300

    lab101 comments

--- lab10/lab101/lab101.srcs/sources_1/new/lab101.sv
+++ lab10/lab101/lab101.srcs/sources_1/new/lab101.sv
@@ -4,7 +4,6 @@ module lab101(
     input logic clk,
     input logic [15 : 0] din,
     output logic [31 : 0] dout
-    //,output logic [23:0] mantissa
     );
     
     logic [31:0] dout_delayed;
@@ -13,6 +12,7 @@ module lab101(
     logic [7:0] exponent;
     logic [31:0] float;
     
+    //задержка выхода на два такта
     always_ff @(posedge clk) begin
         dout <= dout_double_delayed;
         dout_double_delayed <= dout_delayed;
@@ -20,14 +20,18 @@ module lab101(
     end
     
     always_comb begin
-        mantissa = ({16{din[15]}}^din)+din[15]; //if din[15] == 1 => din = ~din + 1
+        // взятие модуля исходного числа как мантиссы
+        mantissa = ({16{din[15]}}^din) + din[15]; //if din[15] == 1 => din = ~din + 1
+
         if (din != 0) begin
-        
-            for(exponent = 8'd127 + 8'd15; mantissa[15] != 1'b1 ;exponent--)
-                mantissa = mantissa << 1;
+            //счёт порядка. начальное значение 127 + 15, т.к. смещение -127 и кол-во бит 16-1
+            //если старший бит не 1, то делается смещение влево, делается до первой 1 в старшем бите
+            for(exponent = 8'd127 + 8'd15; mantissa[15] != 1'b1 ;exponent--) 
+                mantissa = mantissa << 1; 
             
+            //формирование выхода знак, порядок, мантисса и незначащие нули чтобы заполнить все биты мантиссы
             float = {din[15],exponent,mantissa[14:0],8'b00000000};
-        end else float = 32'b0;
+        end else float = 32'b0; //выход задаётся 32 нулями если вход имеет значение 0 
     end
     
     
