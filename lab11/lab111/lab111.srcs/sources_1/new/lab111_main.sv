50318d3e6e065a5edb0969ede6c6432ee651ac0f
олегсий <78318213+lex4z@users.noreply.github.com>
2024-11-11 22:55:03 +0300

    lab111 test

---
+++ lab11/lab111/lab111.srcs/sources_1/new/lab111_main.sv
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+
+module lab111_main #(
+    parameter WINDOW_SIZE = 8
+    )
+    (
+    input logic clk,
+    input logic din_v,
+    input logic [15:0] din,
+    output logic [15:0] dout
+    );
+    logic [23 : 0] accumulator = 24'b0;
+    logic [15 : 0] window [WINDOW_SIZE] = '{default:'0};
+    
+    always_ff @(posedge clk) begin
+        if(din_v)
+            window <= {din,window[0:WINDOW_SIZE-2]};
+            accumulator <= accumulator + din - window[WINDOW_SIZE-1];
+        dout <= accumulator[17:2];
+    end
+    
+    
+    
+endmodule
