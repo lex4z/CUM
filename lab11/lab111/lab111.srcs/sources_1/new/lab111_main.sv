c229d8394e0006a81832ec7ba7eb34554593a745
олегсий <78318213+lex4z@users.noreply.github.com>
2024-11-13 01:01:31 +0300

    Update lab111_main.sv

--- lab11/lab111/lab111.srcs/sources_1/new/lab111_main.sv
+++ lab11/lab111/lab111.srcs/sources_1/new/lab111_main.sv
@@ -9,14 +9,14 @@ module lab111_main #(
     input logic [15:0] din,
     output logic [15:0] dout
     );
-    logic [23 : 0] accumulator = 24'b0;
+    logic [18 : 0] accumulator = 24'b0;
     logic [15 : 0] window [WINDOW_SIZE] = '{default:'0};
     
     always_ff @(posedge clk) begin
         if(din_v)
             window <= {din,window[0:WINDOW_SIZE-2]};
             accumulator <= accumulator + din - window[WINDOW_SIZE-1];
-        dout <= accumulator[17:2];
+        dout <= accumulator >> 3;
     end
     
     
