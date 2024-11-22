6d79b92ad84245ece6a3024c3c97b9e5323be041
олегсий <78318213+lex4z@users.noreply.github.com>
2024-11-22 17:10:01 +0300

    lab12

---
+++ lab12/lab12.srcs/sim_1/new/tb_lab12.sv
@@ -0,0 +1,81 @@
+`timescale 1ns / 1ps
+
+module tb_lab12();
+    localparam int partSumLength = 32;
+    localparam int tapLineLength = 11;
+    
+    logic clk = 1;
+    logic rfd;
+    logic srst;
+    logic din_v;
+    logic signed [15:0] din = 0;
+    logic signed [partSumLength-1:0] dout;
+    
+    
+    //b11: 1 1 1 -1 -1 -1 1 -1 -1 1 -1 
+    //  h: -1 1 -1 -1 1 -1 -1 -1 1 1 1
+    //     0  1  0  0 1  0  0  0 1 1 1
+    logic barker_coeffs [tapLineLength] = {
+    1'b0,
+    1'b1,
+    1'b0,
+    1'b0,
+    1'b1,
+    1'b0,
+    1'b0,
+    1'b0,
+    1'b1,
+    1'b1,
+    1'b1
+    };
+    
+    
+    always #(5) clk = ~clk;
+    
+    ReadFile #(
+        .numOfBits(16),
+        .file_name("/home/user/lab12/data2fpga.dat")
+    ) tb_ReadFile (
+        .clk(clk),
+        .rfd(rfd),
+        .rst(srst),
+        .dv(din_v),
+        .data(din)
+    );
+    
+    tapLine #(
+        .partSumLength (partSumLength),
+        .tapLineLength (tapLineLength)
+    ) test_tapLine (
+        .clk(clk),
+        .sample(din),
+        .sample_valid(din_v),
+        .coeffs (barker_coeffs),
+        .dout(dout)
+    );
+    
+    WriteFile_full #(
+        .numOfBits(partSumLength),
+        .file_name("/home/user/lab12/data_from_fpga.dat")
+    ) tb_WriteFile_full (
+        .clk(clk),
+        .dv(din_v),
+        .DataIn(dout),
+        .sign(1)
+    );
+        
+    initial begin
+        rfd = 0;
+        srst = 1;
+        
+        #100;
+        
+        rfd = 1;
+        srst = 0;
+        
+        
+        
+        #1000000;
+        $stop;
+    end 
+endmodule
---
+++ lab12/lab12.srcs/sources_1/new/u_tap.sv
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+
+module tap #(
+    parameter partSumLength = 20
+)(
+    input logic clk,
+    input logic coeff,
+    input logic signed [15:0] sample,
+    input logic sample_valid,
+    input  logic signed [partSumLength-1:0] prev_tap,
+    output logic signed [partSumLength-1:0] next_tap
+    );
+    
+    always_ff @(posedge clk) begin
+        if(sample_valid) begin
+            if(coeff) next_tap <= $signed(prev_tap) + $signed(sample);
+            else next_tap <=  $signed(prev_tap) -  $signed(sample);
+        end
+    end
+endmodule
