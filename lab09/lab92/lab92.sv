197972b1873745449d6afd002563eb6ab7434361
олегсий <78318213+lex4z@users.noreply.github.com>
2024-12-14 14:00:32 +0300

    Update lab92.sv

--- lab09/lab92/lab92.sv
+++ lab09/lab92/lab92.sv
@@ -9,8 +9,8 @@ module lab92(
     logic signed [15 : 0] result; //(1,16,8)
     
     always_ff @(posedge clk) begin
-        result = x1 * x2;
-        dout = {result[15],result[10:4]};
+        result <= x1 * x2;
+        dout <= {result[15],result[10:4]};
     end
     
 endmodule
