880f2887d3846f42bc8cf1754d784945f1151262
олегсий <78318213+lex4z@users.noreply.github.com>
2024-12-14 14:00:01 +0300

    Update lab93.sv

--- lab09/lab93/lab93.sv
+++ lab09/lab93/lab93.sv
@@ -4,16 +4,16 @@ module lab93(
     input  logic                clk,
     input  logic signed [7 : 0] x1, //(1,8,4)    
     input  logic signed [7 : 0] x2, //(1,8,4)
-    output logic        [7 : 0] dout  
+    output logic signed [7 : 0] dout  
     );
     logic signed [15 : 0] result; //(1,16,8)
     always_ff @(posedge clk) begin
-        result = x1 * x2;
+        result <= x1 * x2;
         
         if (result[14 : 11] == {4{result[15]}}) 
-            dout = result[11 : 4] + result[3]; //если биты 14-11 одинаковы и равны знаковому, то переполнения нету
+            dout <= result[11 : 4] + result[3]; //если биты 14-11 одинаковы и равны знаковому, то переполнения нету
         else 
-            dout = 8'h7F + result[15]; //если знаковый бит 1, то 7F = 01111111 -> 10000000
+            dout <= 8'h7F + result[15]; //если знаковый бит 1, то 7F = 01111111 -> 10000000
         
     end
 endmodule
